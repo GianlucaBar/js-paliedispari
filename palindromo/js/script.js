52a948a00c61bc9d284fa555146f049c2bb52fd5
GianlucaBar <gianlucabarce@gmail.com>
2021-03-25 15:22:10 +0100

    fatto esercizio palindromi

---
+++ palindromo/js/script.js
@@ -0,0 +1,29 @@
+
+// funzione 
+function palindromeCheck(stringa){
+
+    // dichiaro flag = vero 
+    var isPalindroma = true;
+
+// il for deve andare avanti fino a meta' stringa e finche'il flag e'vero   
+    for (i = 0; i < (stringa.length / 2) && isPalindroma == true; i++ ){
+
+        // confronto prima lettera a ultima, seconda a penultima ecc... 
+        if( stringa[i] == stringa[stringa.length - i -1]){
+            // se corrisponde il flag rimane vero e controlla la prossima coppia 
+            isPalindroma = true;
+        } else {
+            // se non corrisponde il flag diventa falso e esce dal ciclo  
+            isPalindroma = false;
+        }
+        // ritorno il valore del flag alla fine del controllo 
+        return isPalindroma;
+    }
+}
+
+// chiedo stringa all'utente 
+var stringaUtente = prompt("scrivi una parola in lettere minuscole");
+
+
+// stampa 
+console.log(palindromeCheck(stringaUtente));
